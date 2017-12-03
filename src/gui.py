6cc4df02db2a352f37279a89872ce7f673fdf507
slimpotatoes <slimpotatoes@bitbucket.org>
2017-12-03 02:53:22 -0500

    SMH simulation module done. Need some clean up and comments

--- src/gui.py
+++ src/gui.py
@@ -64,9 +64,9 @@ class SMGGUI(object):
             np.log1p(self.fft_display(data.SMGData.load(datastruct, 'FTISMHexp'))), cmap='gray')
         self.fig_SMHsim.add_axes(plt.subplot(self.fig_SMHsim.add_subplot(1, 2, 2))).imshow(
             np.log1p(data.SMGData.load(datastruct, 'FTISMHsim')), cmap='gray')
+
         fticsquare = data.SMGData.load(datastruct, 'FTISMHsimDisplay')
-        print('ftsqaure loaded')
-        colormaps = self.generate_colormap_smhsim(fticsquare)
+        colormaps_Moire, colormaps_IC = self.generate_colormap_smhsim(fticsquare)
         ftsmhsim = plt.figure(num='Simulated SMH with colors')
         icsplit = plt.figure(num='Ic split into tiles')
 # A cleaner
@@ -79,15 +79,15 @@ class SMGGUI(object):
         for i in range(0, fticsquare.shape[0]):
             for j in range(0, fticsquare.shape[1]):
                 # Threshold - Color on Moire FFT reconstructed
-                # log_squares_in_HRES = np.log1p(fticsquare[i][j])
-                # mask = log_squares_in_HRES[:, :] > (0.67 * max_general)
-                # log_squares_corrected = log_squares_in_HRES
-                # log_squares_corrected[np.invert(mask)] = np.nan
-                bla = colors.LinearSegmentedColormap.from_list('my_cmap', colormaps[count])
-                #ftsmhsimaxis.imshow(log_squares_corrected, cmap=bla, alpha=.7, clim=(10, 40))
+                Test = np.log1p(fticsquare[i, j])
+                mask = Test[:, :] < (0.67 * max_general)
+                Test[mask] = 0
+                cmap_moire = colors.LinearSegmentedColormap.from_list('my_cmap', colormaps_Moire[count])
+                cmap_ic = colors.LinearSegmentedColormap.from_list('my_cmap', colormaps_IC[count])
+                ftsmhsimaxis.imshow(Test, cmap = cmap_moire, alpha = .7, clim=(10, 40))
                 # Color on split HRES FFT
                 icsplitaxis = icsplit.add_subplot(fticsquare.shape[0], fticsquare.shape[0], count + 1)
-                icsplitaxis.imshow(np.log1p(fticsquare[i, j]), cmap=bla, clim=(27.5, 34.5))
+                icsplitaxis.imshow(np.log1p(fticsquare[i, j]), cmap=cmap_ic, clim=(27.5, 34.5))
                 icsplitaxis.xaxis.set_visible(False)
                 icsplitaxis.yaxis.set_visible(False)
                 count += 1
@@ -105,53 +105,9 @@ class SMGGUI(object):
         customized_color_map_max = []
         for elements in coef_weighted_color:
             customized_color_map_max.append(initial_color_map(elements))
-        customized_color_maps = []
+        customized_color_maps_Moire = []
+        customized_color_maps_Ic =[]
         for elements in customized_color_map_max:
-            customized_color_maps.append([(0, 0, 0, 0.8), elements])
-        return (customized_color_maps)
-
-'''
-# Generate the colormap
-total_tiles_number = numpy.shape(matrix_squares_in_HRES)[0]*numpy.shape(matrix_squares_in_HRES)[1]
-initial_color_map = cm.get_cmap(name="jet")
-coef_weighted_color = numpy.arange(0,total_tiles_number).astype(float)/(total_tiles_number-1)
-customized_color_map_max = []
-for elements in coef_weighted_color:
-    customized_color_map_max.append(initial_color_map(elements))
-customized_color_maps = []
-for elements in customized_color_map_max:
-    customized_color_maps.append([(0,0,0,0.8), elements])
-#print "avant =", customized_color_maps
-#random.shuffle(customized_color_maps)
-#print "apres =",customized_color_maps
-
-
-# Initial image from the stack
-ZERO = numpy.zeros(numpy.shape(matrix_squares_in_HRES[0][0]))
-fig10ax1.imshow(ZERO, cmap = "gray", alpha=1)
-
-max_general = numpy.max(numpy.max(numpy.max(numpy.max(numpy.log1p(matrix_squares_in_HRES)))))
-print max_general
-
-count = 0
-for i in range(0, numpy.shape(matrix_squares_in_HRES)[0]):
-    for j in range (0, numpy.shape(matrix_squares_in_HRES)[1]):
-        # Threshold for fig 10 - Color on Moire FFT reconstructed
-        log_squares_in_HRES = numpy.log1p(matrix_squares_in_HRES[i][j])
-        mask = log_squares_in_HRES[:,:] > (0.67 * max_general)
-        #log_squares_corrected = numpy.multiply(log_squares,index)
-        log_squares_corrected = log_squares_in_HRES
-        log_squares_corrected[numpy.invert(mask)] = numpy.nan
-        bla = colors.LinearSegmentedColormap.from_list('my_cmap', customized_color_maps[count])
-        fig10ax1.imshow(log_squares_corrected, cmap = bla, alpha = .7, clim=(10,40))
-        #fig10ax1.imshow(log_squares_corrected, cmap = bla, alpha = .5, clim=(29.5,31.5))
-        # Color on split HRES FFT
-        fig11ax1 = fig11.add_subplot(number_square_per_lines_corr, number_square_per_lines_corr, count+1)
-        fig11ax1.imshow(numpy.log1p(matrix_squares_in_HRES[i, j]), cmap=bla, clim=(27.5,34.5))
-        fig11ax1.xaxis.set_visible(False)
-        fig11ax1.yaxis.set_visible(False)
-        #fig12ax1.hist(numpy.log1p(matrix_squares_in_HRES[i, j]), bins=10, normed=True)
-        count += 1
-        #print "count=", count
-plt.draw()
-'''
+            customized_color_maps_Moire.append([(0, 0, 0, 0.0), elements])
+            customized_color_maps_Ic.append([(0, 0, 0, 0.8), elements])
+        return customized_color_maps_Moire, customized_color_maps_Ic
