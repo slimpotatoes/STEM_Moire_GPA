4f06d7d443fe634a6f65595812a7e2ed58cdd489
slimpotatoes <slimpotatoes@bitbucket.org>
2017-12-14 23:50:17 -0500

    Implementation of Test #4

--- src/test_gpa.py
+++ src/test_gpa.py
@@ -1,6 +1,7 @@
 # Test of GPA Module
 
 import numpy as np
+import math as math
 import data as data
 import gpa as gpa
 import matplotlib.pyplot as plt
@@ -47,19 +48,25 @@ gpa.gpa('Mask1', data_test_2)
 
 # Input/Output data - Phase is supposed to be constant and equal to 0, anything different from 0 represents the error.
 fig_test2 = plt.figure()
-fig_test2.add_subplot(2, 2, 1).imshow(ismh2, cmap='gray')
-fig_test2.add_subplot(2, 2, 2).imshow(np.log1p(np.fft.fftshift(np.abs(ft_ismh2 ** 2))), cmap='gray')
-fig_test2.add_subplot(2, 2, 3).imshow(data_test_2.load_g('Mask1', 'phaseraw'), cmap='gray', vmin=-np.pi, vmax=np.pi)
-fig_test2.add_subplot(2, 2, 4).imshow(data_test_2.load_g('Mask1', 'phasegM'), cmap='gray', vmin=-np.pi, vmax=np.pi)
+fig_test2.add_subplot(2, 3, 1).imshow(ismh2, cmap='gray')
+fig_test2.add_subplot(2, 3, 4).imshow(np.log1p(np.fft.fftshift(np.abs(ft_ismh2 ** 2))), cmap='gray')
+fig_test2.add_subplot(2, 3, 2).imshow(data_test_2.load_g('Mask1', 'phaseraw'), cmap='gray', vmin=-np.pi, vmax=np.pi)
+fig_test2.add_subplot(2, 3, 5).imshow(data_test_2.load_g('Mask1', 'phasegM'), cmap='gray', vmin=-np.pi, vmax=np.pi)
+fig_test2.add_subplot(2, 3, 3).imshow(data_test_2.load_g('Mask1', 'deltagM')[0], cmap='gray', vmin=-1, vmax=1)
+fig_test2.add_subplot(2, 3, 6).imshow(data_test_2.load_g('Mask1', 'deltagM')[1], cmap='gray', vmin=-1, vmax=1)
 
 # Input/Output data in 1D
 fig_test2_1d = plt.figure()
-fig_test2_1d.add_subplot(2, 2, 1).plot(ismh2[128, :])
-fig_test2_1d.add_subplot(2, 2, 2).plot(x2, np.log1p(np.fft.fftshift(np.abs(ft_ismh2 ** 2)))[128, :])
-fig_test2_1d.add_subplot(2, 2, 3).plot(x2, data_test_2.load_g('Mask1', 'phaseraw')[128, :])
+fig_test2_1d.add_subplot(2, 3, 1).plot(ismh2[128, :])
+fig_test2_1d.add_subplot(2, 3, 4).plot(x2, np.log1p(np.fft.fftshift(np.abs(ft_ismh2 ** 2)))[128, :])
+fig_test2_1d.add_subplot(2, 3, 2).plot(x2, data_test_2.load_g('Mask1', 'phaseraw')[128, :])
 plt.ylim(-np.pi, np.pi)
-fig_test2_1d.add_subplot(2, 2, 4).plot(x2, data_test_2.load_g('Mask1', 'phasegM')[128, :])
+fig_test2_1d.add_subplot(2, 3, 5).plot(x2, data_test_2.load_g('Mask1', 'phasegM')[128, :])
 plt.ylim(-np.pi, np.pi)
+fig_test2_1d.add_subplot(2, 3, 3).plot(x2, data_test_2.load_g('Mask1', 'deltagM')[0, 128, :])
+plt.ylim(-1, 1)
+fig_test2_1d.add_subplot(2, 3, 6).plot(x2, data_test_2.load_g('Mask1', 'deltagM')[1, 128, :])
+plt.ylim(-1, 1)
 
 plt.show()
 
@@ -69,18 +76,18 @@ plt.show()
 
 data_test_3 = data.SMGData()
 
-# STEM Moire hologram with a unique periodicity of 4 pixels along horizontal axis (easy case) !!
+# STEM Moire hologram with a unique periodicity of 4 pixels along horizontal axis on half image and a periodicity of
+# 4.5 pixel along the same axis on the second half of the image (easy case). This leads to o a 'delta g' of
+# 1/4-1/4.5 = 0.0277778.
 
 x3a = np.linspace(0, 127, 128)
 x3b = np.linspace(128, 255, 128)
 y3 = np.linspace(0, 255, 256)
 mx3a, my3a = np.meshgrid(x3a, y3)
 mx3b, my3b = np.meshgrid(x3b, y3)
-ismh3a = np.sin(mx3a * np.pi / 2)
+ismh3a = np.sin(mx3a * 2 * np.pi / 4)
 ismh3b = np.sin(mx3b * 2 * np.pi / 4.5)
 ismh3 = np.concatenate((ismh3a, ismh3b), axis=1)
-print(ismh3)
-print(ismh3.shape)
 ft_ismh3 = np.fft.fft2(ismh3)
 
 # Store data in datastructure
@@ -109,18 +116,85 @@ gpa.gpa('Mask1', data_test_3)
 
 # Input/Output data - Phase is supposed to be constant and equal to 0, anything different from 0 represents the error.
 fig_test3 = plt.figure()
-fig_test3.add_subplot(2, 2, 1).imshow(ismh3, cmap='gray')
-fig_test3.add_subplot(2, 2, 2).imshow(np.log1p(np.fft.fftshift(np.abs(ft_ismh3 ** 2))), cmap='gray')
-fig_test3.add_subplot(2, 2, 3).imshow(data_test_3.load_g('Mask1', 'phaseraw'), cmap='gray', vmin=-np.pi, vmax=np.pi)
-fig_test3.add_subplot(2, 2, 4).imshow(data_test_3.load_g('Mask1', 'phasegM'), cmap='gray', vmin=-np.pi, vmax=np.pi)
+fig_test3.add_subplot(2, 3, 1).imshow(ismh3, cmap='gray')
+fig_test3.add_subplot(2, 3, 4).imshow(np.log1p(np.fft.fftshift(np.abs(ft_ismh3 ** 2))), cmap='gray')
+fig_test3.add_subplot(2, 3, 2).imshow(data_test_3.load_g('Mask1', 'phaseraw'), cmap='gray', vmin=-np.pi, vmax=np.pi)
+fig_test3.add_subplot(2, 3, 5).imshow(data_test_3.load_g('Mask1', 'phasegM'), cmap='gray', vmin=-np.pi, vmax=np.pi)
+fig_test3.add_subplot(2, 3, 3).imshow(data_test_3.load_g('Mask1', 'deltagM')[0], cmap='gray', vmin=-1, vmax=1)
+fig_test3.add_subplot(2, 3, 6).imshow(data_test_3.load_g('Mask1', 'deltagM')[1], cmap='gray', vmin=-1, vmax=1)
 
 # Input/Output data in 1D
 fig_test3_1d = plt.figure()
-fig_test3_1d.add_subplot(2, 2, 1).plot(ismh3[128, :])
-fig_test3_1d.add_subplot(2, 2, 2).plot(x2, np.log1p(np.fft.fftshift(np.abs(ft_ismh3 ** 2)))[128, :])
-fig_test3_1d.add_subplot(2, 2, 3).plot(x2, data_test_3.load_g('Mask1', 'phaseraw')[128, :])
+fig_test3_1d.add_subplot(2, 3, 1).plot(ismh3[128, :])
+fig_test3_1d.add_subplot(2, 3, 4).plot(x2, np.log1p(np.fft.fftshift(np.abs(ft_ismh3 ** 2)))[128, :])
+fig_test3_1d.add_subplot(2, 3, 2).plot(x2, data_test_3.load_g('Mask1', 'phaseraw')[128, :])
 plt.ylim(-np.pi, np.pi)
-fig_test3_1d.add_subplot(2, 2, 4).plot(x2, data_test_3.load_g('Mask1', 'phasegM')[128, :])
+fig_test3_1d.add_subplot(2, 3, 5).plot(x2, data_test_3.load_g('Mask1', 'phasegM')[128, :])
 plt.ylim(-np.pi, np.pi)
+fig_test3_1d.add_subplot(2, 3, 3).plot(x2, data_test_3.load_g('Mask1', 'deltagM')[0, 128, :])
+plt.ylim(-1, 1)
+fig_test3_1d.add_subplot(2, 3, 6).plot(x2, data_test_3.load_g('Mask1', 'deltagM')[1, 128, :])
+plt.ylim(-1, 1)
+
+plt.show()
+
+# #######################################
+# Test #4 in TestPlan document
+# #######################################
+
+data_test_4 = data.SMGData()
+
+# STEM Moire hologram with a unique periodicity of 4 pixels along horizontal axis on half image and a periodicity of
+# 4.5 pixel along the same axis on the second half of the image (easy case). This leads to o a 'delta g' of
+# 1/4-1/4.5 = 0.0277778.
+
+x4a = np.linspace(0, 127, 128)
+x4b = np.linspace(128, 255, 128)
+y4 = np.linspace(0, 255, 256)
+mx4a, my4a = np.meshgrid(x4a, y4)
+mx4b, my4b = np.meshgrid(x4b, y4)
+ismh4a = np.sin(mx4a * 2 * np.pi / 4)
+ismh4b = np.sin(mx4b * 2 * np.pi / 4.5)
+ismh4 = np.concatenate((ismh4a, ismh4b), axis=1)
+ft_ismh4 = np.fft.fft2(ismh4)
+
+# Store data in datastructure
+
+data_test_4.store('ISMHexp', ismh4)
+data_test_4.store('FTISMHexp', ft_ismh4)
+
+# Mask parameter
+
+r4 = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+center4 = (192, 128)
+
+# Create the amount of branches corresponding the the radius tested and store the mask properties
+for radius in r4:
+    data_test_4.create_branch(str(radius))
+    circle4 = center4, radius
+    data_test_4.store_g(str(radius), 'Mask', circle4)
+
+
+# Looping on the various radius into gpa
+for radius in r4:
+    gpa.gpa(str(radius), data_test_4)
+
+count = 1
+fig_test4 = plt.figure()
+fig_test4_1d = plt.figure()
+fig_test4_deltag_1d = plt.figure()
+N_sublpot = math.ceil(math.sqrt(len(r4)))
+print(math.ceil(math.sqrt(len(r4))))
+for element in r4:
+    fig_test4.add_subplot(N_sublpot, N_sublpot, count).imshow(data_test_4.load_g(
+        str(element), 'phasegM'), cmap='gray', vmin=-np.pi, vmax=np.pi)
+    fig_ax = fig_test4_1d.add_subplot(N_sublpot, N_sublpot, count)
+    fig_ax.plot(x2, data_test_4.load_g(str(element), 'phasegM')[128, :])
+    fig_ax.set_ylim(-np.pi, np.pi)
+    fig_ax_g = fig_test4_deltag_1d.add_subplot(N_sublpot, N_sublpot, count)
+    fig_ax_g.plot(x2, data_test_4.load_g(
+        str(element), 'deltagM')[1, 128, :])
+    fig_ax_g.set_ylim(-0.1, 0.1)
+    count += 1
 
 plt.show()
