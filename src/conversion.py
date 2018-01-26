66346cbf0fc7e67feeb7b05388897ef5c749cdd2
slimpotatoes <slimpotatoes@bitbucket.org>
2018-01-26 13:22:21 -0500

    Correction in documentation

--- src/conversion.py
+++ src/conversion.py
@@ -9,17 +9,11 @@ import math as math
 def conversion(mask_id, n, m, datastruct):
     p = data.SMGData.load(datastruct, 'p')
     g_m_uns = data.SMGData.load_g(datastruct, mask_id, 'gMuns')
-    print('g M ref avant calib')
-    print(g_m_uns)
     g_m_uns[0, :, :] = 1 / p * g_m_uns[0, :, :]
     g_m_uns[1, :, :] = 1 / p * g_m_uns[1, :, :]
-    print('g M ref apres calib')
-    print(g_m_uns)
     correction = np.ones(g_m_uns.shape)
     correction[0, :, :] = - m / p * correction[0, :, :]
     correction[1, :, :] = n / p * correction[1, :, :]
-    print('correction')
-    print(correction)
     g_c_uns = g_m_uns + correction
     data.SMGData.store_g(datastruct, mask_id, 'gCuns', g_c_uns)
     norm = math.sqrt(g_c_uns[0, 0, 0] ** 2 + g_c_uns[1, 0, 0] ** 2)
---
+++ src/test_conversion.py
@@ -0,0 +1,6 @@
+import data
+
+datastruct = data.SMGData()
+datastruct.create_branch('Mask1')
+datastruct.create_branch('Mask2')
+
