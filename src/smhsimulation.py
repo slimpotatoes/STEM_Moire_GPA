29ae16c097aca705440c3525e135fddf2a5e55eb
slimpotatoes <slimpotatoes@bitbucket.org>
2018-02-20 17:51:01 -0500

    Adding the case n_lim is "pair"

--- src/smhsimulation.py
+++ src/smhsimulation.py
@@ -17,36 +17,60 @@ def smh_sim(datastruct):
     ft_ic = np.fft.fftshift(np.abs(np.fft.fft2(data.SMGData.load(datastruct, 'ICref')) ** 2))
     p = data.SMGData.load(datastruct, 'p')
     pref = data.SMGData.load(datastruct, 'pref')
-    n_lim = math.floor(p/(pref))
+    n_lim = math.floor(p/pref)
     fov = ft_ic.shape[0]
     tile = int(pref / p * fov)
     ft_ismh_sim = np.ndarray(ft_ic.shape)
-    ft_ic_square = np.ndarray((len(range(0, n_lim)), len(range(0, n_lim)), tile, tile))
-
-    # Print statement to inform user
-    print('Shape in pixel of refence: ', ft_ismh_sim.shape)
-    print('Shape in pixel of the tile: ', ft_ic_square.shape)
-    print('Tile size (in pixel): ', tile)
-    print('Number of tiles: ', (n_lim) ** 2)
-    print('Please wait the calculation can take some time !!!')
 
     # Simulation by calculating the STEM Moire hologram equation.
     counter = 0
-    initial_coordinate = (int(0.5 * (fov - n_lim * tile)), int(0.5 * (fov - n_lim * tile)))
-    for i in range(0, n_lim):
-        for j in range(0, n_lim):
-            ft_ismh_sim += shift(ft_ic, [int(i - 0.5 * n_lim) * tile, int(j - 0.5 * n_lim) * tile],
-                                 cval=0, order=0, prefilter=False)
-            a = int(initial_coordinate[0] + i * tile)
-            b = a + tile
-            c = int(initial_coordinate[0] + j * tile)
-            d = c + tile
-            ft_ic_square[i][j] = ft_ic[a:b, c:d]
-            counter = counter + 1
-            # print('Loop number: ', counter)
+    if n_lim % 2 != 0:
+        initial_coordinate = (int(0.5 * (fov - n_lim * tile)), int(0.5 * (fov - n_lim * tile)))
+        ft_ic_square = np.ndarray((len(range(0, n_lim)), len(range(0, n_lim)), tile, tile))
+
+        # Print statement to inform user
+        print('Shape in pixel of refence: ', ft_ismh_sim.shape)
+        print('Shape in pixel of the tile: ', ft_ic_square.shape)
+        print('Tile size (in pixel): ', tile)
+        print('Number of tiles: ', n_lim ** 2)
+        print('Please wait the calculation can take some time !!!')
+
+        for i in range(0, n_lim):
+            for j in range(0, n_lim):
+                ft_ismh_sim += shift(ft_ic, [int(i - 0.5 * n_lim) * tile, int(j - 0.5 * n_lim) * tile],
+                                     cval=0, order=0, prefilter=False)
+                a = int(initial_coordinate[0] + i * tile)
+                b = a + tile
+                c = int(initial_coordinate[0] + j * tile)
+                d = c + tile
+                ft_ic_square[i][j] = ft_ic[a:b, c:d]
+                counter += 1
+                # print('Loop number: ', counter)
+    else:
+        n_lim -= 1
+        initial_coordinate = (int(0.5 * (fov - n_lim * tile)), int(0.5 * (fov - n_lim * tile)))
+        ft_ic_square = np.ndarray((len(range(0, n_lim)), len(range(0, n_lim)), tile, tile))
+
+        # Print statement to inform user
+        print('Shape in pixel of refence: ', ft_ismh_sim.shape)
+        print('Shape in pixel of the tile: ', ft_ic_square.shape)
+        print('Tile size (in pixel): ', tile)
+        print('Number of tiles: ', n_lim ** 2)
+
+        print('Please wait the calculation can take some time !!!')
+        for i in range(0, n_lim):
+            for j in range(0, n_lim):
+                ft_ismh_sim += shift(ft_ic, [int(i - 0.5 * n_lim) * tile, int(j - 0.5 * n_lim) * tile],
+                                     cval=0, order=0, prefilter=False)
+                a = int(initial_coordinate[0] + i * tile)
+                b = a + tile
+                c = int(initial_coordinate[0] + j * tile)
+                d = c + tile
+                ft_ic_square[i][j] = ft_ic[a:b, c:d]
+                counter += 1
     print('Simulation done')
 
-    #Storing data into data structure.
+    # Storing data into data structure.
     data.SMGData.store(datastruct, 'FTISMHsim', ft_ismh_sim[
                         int(0.5*(ft_ismh_sim.shape[0] - tile)):int(0.5*(ft_ismh_sim.shape[0] + tile)),
                         int(0.5*(ft_ismh_sim.shape[0] - tile)):int(0.5*(ft_ismh_sim.shape[0] + tile))])
